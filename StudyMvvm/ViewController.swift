94c625687be5737e1f6a38fc567086b05599f243
Hiệp <ch.dhiep@gmail.com>
2022-12-28 15:24:34 +0700

    Initial Commit

---
+++ StudyMvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyMvvm
+//
+//  Created by Châu Hiệp on 28/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
